dd5003b5861ce17f0ad11abe88721e5444383d2b
muramoto <b1023213@fun.ac.jp>
2025-06-21 00:49:56 +0900

    これで動くようになります。

--- app_1/lib/main.dart
+++ app_1/lib/main.dart
@@ -5,8 +5,9 @@ import 'package:app_1/view/odato/niku.dart';
 import 'package:app_1/view/togasi/memo.dart';
 import 'package:app_1/view/rikiya/nikki.dart';
 
-
-import '';
+void main() {
+  runApp(const MaterialApp(home: MyApp()));
+}
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -19,9 +20,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('MyApp'),
-      ),
+      appBar: AppBar(title: const Text('MyApp')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -35,35 +34,42 @@ class _MyAppState extends State<MyApp> {
               },
               child: const Text('Button 1'),
             ),
-            ElevatedButton(onPressed: () {
-              Navigator.push(
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const TodoPage()),
                 );
-                }, child: const Text('Button 2')
-          ),
+              },
+              child: const Text('Button 2'),
+            ),
             ElevatedButton(
               onPressed: () {
-              Navigator.push(
+                Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const Nikki()),
                 );
-            }, child: const Text('Button 2')),
+              },
+              child: const Text('Button 2'),
+            ),
             ElevatedButton(
               onPressed: () {
-              Navigator.push(
+                Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const Memo()),
                 );
-            }, child: const Text('Button 3')),
-            ElevatedButton(onPressed: () {
-              Navigator.push(
+              },
+              child: const Text('Button 3'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const Niku()),
                 );
-            }, child: const Text('Button 4')),
-           
-                
+              },
+              child: const Text('Button 4'),
+            ),
           ],
         ),
       ),
