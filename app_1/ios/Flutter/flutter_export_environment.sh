7553edba2cbc5ac58f985e6098526b88c27b30ab
toga <b1023157@fun.ac.jp>
2025-06-30 00:45:03 +0900

    メモページ

--- app_1/ios/Flutter/flutter_export_environment.sh
+++ app_1/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\akaka\skill_group\app_1"
+export "FLUTTER_ROOT=C:\Users\togat\my_web_app\.vscode\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\togat\Documents\togashi_skill\skill_group\app_1"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- app_1/lib/view/togasi/memo.dart
+++ app_1/lib/view/togasi/memo.dart
@@ -1,21 +1,147 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Memo extends StatelessWidget {
+class Memo extends StatefulWidget {
   const Memo({super.key});
+
+  @override
+  State<Memo> createState() => _MemoState();
+}
+
+class _MemoState extends State<Memo> {
+  List<String> _memos = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMemos();
+  }
+
+  Future<void> _loadMemos() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _memos = prefs.getStringList('memos') ?? [];
+    });
+  }
+
+  Future<void> _saveMemos() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('memos', _memos);
+  }
+
+  Future<void> _addMemo() async {
+    final newMemo = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const MemoDetailPage()),
+    );
+    if (newMemo != null && newMemo is String && newMemo.isNotEmpty) {
+      setState(() {
+        _memos.add(newMemo);
+      });
+      await _saveMemos();
+    }
+  }
+
+  Future<void> _editMemo(int index) async {
+    final updatedMemo = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MemoDetailPage(initialText: _memos[index]),
+      ),
+    );
+    if (updatedMemo != null &&
+        updatedMemo is String &&
+        updatedMemo.isNotEmpty) {
+      setState(() {
+        _memos[index] = updatedMemo;
+      });
+      await _saveMemos();
+    }
+  }
+
+  Future<void> _deleteMemo(int index) async {
+    setState(() {
+      _memos.removeAt(index);
+    });
+    await _saveMemos();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Memo')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text('This is the Memo page'),
-            SizedBox(height: 20),
-            Text('No memos available'),
-          ],
+      body: _memos.isEmpty
+          ? const Center(
+              child: Text(
+                'No memos available',
+                style: TextStyle(color: Colors.blue),
+              ),
+            )
+          : ListView.builder(
+              itemCount: _memos.length,
+              itemBuilder: (context, index) => ListTile(
+                title: Text(
+                  _memos[index],
+                  style: const TextStyle(color: Colors.blue),
+                ),
+                onTap: () => _editMemo(index),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete, color: Colors.red),
+                  onPressed: () => _deleteMemo(index),
+                ),
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addMemo,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class MemoDetailPage extends StatefulWidget {
+  final String? initialText;
+  const MemoDetailPage({this.initialText, super.key});
+
+  @override
+  State<MemoDetailPage> createState() => _MemoDetailPageState();
+}
+
+class _MemoDetailPageState extends State<MemoDetailPage> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: widget.initialText ?? '');
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('メモ詳細')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          controller: _controller,
+          maxLines: null,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'メモ内容',
+          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context, _controller.text);
+        },
+        child: const Icon(Icons.save),
       ),
     );
   }
